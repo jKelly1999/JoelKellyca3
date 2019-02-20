d1537e6d1dfb2ae3f600d969aba7b172d28b2c46
jKelly1999 <d00203711@student.dkit.ie>
2019-02-20 09:49:42 +0000

    This is a small page that includes things such as the title, header, and css link for all pages. It is included in pages such as vehicles_list.php. It has been tested and is working.

---
+++ view/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>Joel Dyas Kelly CA3</title>
+    <link href="../main.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>Car Reviews</h1></header>
+       
